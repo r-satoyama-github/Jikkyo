6950944de28905130df8f5138d49808ba2e72ac2
ryoichi <ryoichi.satoyama@gmail.com>
2024-05-03 00:16:40 +0900

    add voices

--- main.py
+++ main.py
@@ -4,6 +4,60 @@ import streamlit as st
 
 st.title("Jikkyo")
 
+# クルマ 
+st.write("GRヤリス")
+st.audio("voices/GRヤリス.mp3", format="audio/mpeg")
+st.write("GT-R")
+st.audio("voices/GT-R.mp3", format="audio/mpeg")
+
+# スタート
+st.write("スタートラインに立ちました")
+st.audio("voices/スタートラインに立ちました.mp3", format="audio/mpeg")
+st.write("スタート音")
+st.audio("voices/スタート音.mp3", format="audio/mpeg")
+
+# トラブル
 st.write("あ〜っと大クラッシュ")
-st.audio("big_crash.mp3", format="audio/mpeg")
-st.audio("big_crash.m4a", format="audio/mpeg")
+st.audio("voices/あ〜っと大クラッシュ.mp3", format="audio/mpeg")
+st.write("大クラッシュ(一心ver)")
+st.audio("voices/大クラッシュ(一心ver).mp3", format="audio/mpeg")
+st.write("大クラッシュ2(一心ver)")
+st.audio("voices/大クラッシュ2(一心ver).mp3", format="audio/mpeg")
+st.write("あ〜っとスピンだ")
+st.audio("voices/あ〜っとスピンだ.mp3", format="audio/mpeg")
+st.write("あ〜っと接触だ")
+st.audio("voices/あ〜っと接触だ.mp3", format="audio/mpeg")
+st.write("オーバーランです")
+st.audio("voices/オーバーランです.mp3", format="audio/mpeg")
+
+# コメント
+st.write("タイヤ交換を行います")
+st.audio("voices/タイヤ交換を行います.mp3", format="audio/mpeg")
+st.write("なにしてまんねん")
+st.audio("voices/なにしてまんねん.mp3", format="audio/mpeg")
+st.write("はやい")
+st.audio("voices/はやい.mp3", format="audio/mpeg")
+st.write("はやいぞ")
+st.audio("voices/はやいぞ.mp3", format="audio/mpeg")
+st.write("はやいぞ(一心ver)")
+st.audio("voices/はやいぞ(一心ver).mp3", format="audio/mpeg")
+st.write("ピットイン")
+st.audio("voices/ピットイン.mp3", format="audio/mpeg")
+st.write("速い、速すぎる")
+st.audio("voices/速い、速すぎる.mp3", format="audio/mpeg")
+st.write("追いかける")
+st.audio("voices/追いかける.mp3", format="audio/mpeg")
+st.write("追い抜いた")
+st.audio("voices/追い抜いた.mp3", format="audio/mpeg")
+st.write("抜いた")
+st.audio("voices/抜いた.mp3", format="audio/mpeg")
+st.write("飛んだ")
+st.audio("voices/飛んだ.mp3", format="audio/mpeg")
+st.write("並んだ")
+st.audio("voices/並んだ.mp3", format="audio/mpeg")
+
+# ゴール
+st.write("ゴール")
+st.audio("voices/ゴール.mp3", format="audio/mpeg")
+st.write("ゴール2")
+st.audio("voices/ゴール2.mp3", format="audio/mpeg")
